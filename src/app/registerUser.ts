f5ab0055877f3bc6268ef2b39e809b3028c3ffd2
Felipe Brunetti <felipe.brunetti94@gmail.com>
2021-01-10 20:14:34 -0300

    feat(application): add registerUser app

--- src/app/registerUser.ts
+++ src/app/registerUser.ts
@@ -1,3 +1,14 @@
-export default () => {
-    console.log("register")
+import { Request, Response } from 'express'
+import * as User from '../domain/user'
+import * as userRepository from '../infra/userRepository'
+
+export default (req: Request, res: Response): void => {
+  try {
+    const userData = req.body
+    const user = User.create(userData)
+    userRepository.register(user)
+    res.end(`user: ${user.username} saved with success`)
+  } catch (error) {
+    res.end('There is been a problem with yout request')
+  }
 }
