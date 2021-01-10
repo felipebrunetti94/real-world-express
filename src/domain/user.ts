fedbef1d864901fc5d11ab28149abf0a63386c93
Felipe Brunetti <felipe.brunetti94@gmail.com>
2021-01-10 20:14:14 -0300

    feat(domain): add user domain

--- src/domain/user.ts
+++ src/domain/user.ts
@@ -1,7 +1,33 @@
-interface IUserAuth {
-    email: String
-    password: String
+export interface IUserRequestData {
+  username: string
+  email: string
+  password: string
 }
 
-const isAuthValid = (userAuth: IUserAuth): Boolean => 
-    userAuth.email !== '' && userAuth.password !== ''
+export interface IUser {
+  username: string
+  bio: string
+  image: null | string
+}
+
+export interface IUserAuth extends IUser {
+  token: string
+  email: string
+  password: string
+}
+
+export const validate = (): null => null
+
+export const create = (userData: IUserRequestData): IUserAuth => {
+  return {
+    username: userData.username,
+    email: userData.email,
+    token: '',
+    bio: '',
+    image: null,
+    password: userData.password,
+  }
+}
+
+export const isAuthValid = (userAuth: IUserAuth): boolean =>
+  userAuth.email !== '' && userAuth.password !== ''
